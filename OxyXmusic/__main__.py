048ff062e9e9570de62fe8315129c4ce38f332e3
OxyNotOp <OxyNotOp@users.noreply.github.com>
2021-07-21 09:34:17 +0000

    Replaced new name

--- OxyXmusic/__main__.py
+++ OxyXmusic/__main__.py
@@ -1,5 +1,5 @@
 # OxyXmusic (Telegram bot project)
-# Copyright (C) 2021  Inuka Asith & Rojserbest
+# Copyright (C) 2021  OxyNotOp & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
