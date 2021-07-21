92ce13cec396f637b2ff7e70bec70d9f0a58ffa1
OxyNotOp <OxyNotOp@users.noreply.github.com>
2021-07-21 09:36:59 +0000

    Replaced new name

--- OxyXmusic/modules/play.py
+++ OxyXmusic/modules/play.py
@@ -1,5 +1,5 @@
 # OxyXmusic (Telegram bot project)
-# Copyright (C) 2021  Inukaasith
+# Copyright (C) 2021  OxyNotOp
 # Copyright (C) 2021  TheHamkerCat (Python_ARQ)
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
